310eb1365c80180d797adec98e21c40ce70750ba
xiaoyueyue165 <1656800216@qq.com>
2019-11-14 00:40:17 +0800

    add React todos

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# todomvc-demos
-angular1.x，vue，react
+# todolist
+
+> React • TodoMVC
+
+这仅仅是一个模板，可以直接使用
+
+## 技术点
+
+- 1. 获取数据并显示到页面中 ok
+- 2. 添加任务 ok
+- 3. 删除任务（单个）ok
+- 4. 更改任务状态 ok
+- 5. 增加筛选 ok
+- 7. 计算未完成任务的数量 ok
+- 8. 清空已完成任务 ok
+- 9. 批量更改任务状态 ok
+- 10. 修改任务名称 ok
+
+##### 参考
+
+- https://github.com/tastejs/todomvc
+- https://github.com/tastejs/todomvc-app-template
---
+++ src/components/TodoList.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'onShow'
+    };
+    this.deleteItem = this.deleteItem.bind(this);
+    this.completeItem = this.completeItem.bind(this);
+  }
+  changeInputMode(id, mode) {
+    this.props.changeValMode(id, mode);
+  }
+  deleteItem(id) {
+    this.props.deleteOneById(id);
+  }
+  completeItem(id) {
+    this.props.completeOneById(id);
+  }
+
+  showResulData(data, type) {
+    if (type === 'active') {
+      return data.filter(item => {
+        return item.isCompleted === false;
+      });
+    }
+    if (type === 'completed') {
+      return data.filter(item => {
+        return item.isCompleted === true;
+      });
+    }
+    return data;
+  }
+  render() {
+    console.log('render');
+    const { data, type } = this.props;
+    const { mode } = this.state;
+    const result = this.showResulData(data, type);
+
+    return (
+      <ul className="todo-list">
+        {result.map(item => (
+          <li
+            className={item.isCompleted === true ? 'completed' : ''}
+            key={item.id}
+          >
+            {item.mode == 'onShow' ? (
+              <div class="view">
+                <input
+                  onClick={this.completeItem.bind(this, item.id)}
+                  className="toggle"
+                  type="checkbox"
+                  checked={item.isCompleted ? 'checked' : ''}
+                />
+                <label
+                  onDoubleClick={() => this.changeInputMode(item.id, item.mode)}
+                >
+                  {item.name}
+                </label>
+                <button
+                  className="destroy"
+                  onClick={this.deleteItem.bind(this, item.id)}
+                ></button>
+              </div>
+            ) : (
+              ''
+            )}
+
+            {item.mode == 'onEdit' ? (
+              <input
+                className="edit"
+                style={{ display: 'block' }}
+                value={item.name}
+                onChange={e => this.props.editItemById(item.id, e.target.value)}
+                onBlur={() => this.props.fnishEdit(item.id)}
+              />
+            ) : (
+              ''
+            )}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default TodoList;
