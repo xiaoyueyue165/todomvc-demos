53bf7a9849c0e660719c20043ded6fa29946f45f
xiaoyueyue165 <1656800216@qq.com>
2019-11-14 00:51:35 +0800

    add React todos

--- angular1.x/src/app.js
+++ angular1.x/src/app.js
@@ -1,120 +1,106 @@
-angular.module('myApp', [])
+angular
+  .module("myApp", [])
 
-	.controller('demoCtrl', demoCtrl)
+  .controller("demoCtrl", demoCtrl);
 
 function demoCtrl($scope, $http) {
+  // 1.获取任务并显示到页面中
+  $scope.taskList = [];
 
-	// 1.获取任务并显示到页面中
-	$scope.taskList = [];
+  $http({
+    url: "https://yanyue404.github.io/todomvc-demos/static/data.json",
+    method: "get"
+  }).then(function(res) {
+    for (var i = 0; i < res.data.length; i++) {
+      res.data[i].isEdit = false;
+    }
 
-	$http({
-		url: 'https://xiaoyueyue165.github.io/todomvc-demos/static/data.json',
-		method: 'get'
-	}).then(function (res) {
+    $scope.taskList = res.data;
+    console.log(res.data);
+  });
 
-		for (var i = 0; i < res.data.length; i++) {
-			res.data[i].isEdit = false;
-		}
+  // 2.添加任务
+  $scope.submitTask = function() {
+    $scope.taskList.push({
+      id: Math.random(),
+      name: $scope.task,
+      isCompleted: false
+    });
+    $scope.task = "";
+  };
+  // 3.删除单个任务
+  $scope.deleteTask = function(id) {
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      if ($scope.taskList[i].id == id) {
+        $scope.taskList.splice(i, 1);
+      }
+    }
+  };
 
-		$scope.taskList = res.data;
-		console.log(res.data)
+  // 4.更改任务状态
+  //(1).将复选框和数据做关联
+  //(2).将数据和类名做关联(就是利用数据的值[true|false] 来决定是否添加类名)
 
-	})
+  // 5.增加筛选功能
+  $scope.condition = "";
 
-	// 2.添加任务
-	$scope.submitTask = function () {
+  $scope.filterTask = function(type) {
+    switch (type) {
+      case "All":
+        $scope.condition = "";
+        break;
 
-		$scope.taskList.push({
-			id: Math.random(),
-			name: $scope.task,
-			isCompleted: false
-		});
-		$scope.task = '';
-	}
-	// 3.删除单个任务
-	$scope.deleteTask = function (id) {
-		for (var i = 0; i < $scope.taskList.length; i++) {
-			if ($scope.taskList[i].id == id) {
-				$scope.taskList.splice(i, 1);
-			}
-		}
-	}
+      case "Active":
+        $scope.condition = false;
+        console.log("筛选结果" + $scope.condition);
+        break;
 
-	// 4.更改任务状态
-	//(1).将复选框和数据做关联
-	//(2).将数据和类名做关联(就是利用数据的值[true|false] 来决定是否添加类名)
+      case "Completed":
+        $scope.condition = true;
+        break;
+    }
+  };
 
-	// 5.增加筛选功能
-	$scope.condition = '';
+  // 6.计算未完成的数量
+  $scope.calcNumber = function() {
+    var result = 0;
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      if (!$scope.taskList[i].isCompleted) {
+        result++;
+      }
+    }
+    return result;
+  };
+  // 7.清空已完成的任务
+  $scope.clearData = function() {
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      if ($scope.taskList[i].isCompleted) {
+        $scope.taskList.splice(i, 1);
+        i--;
+      }
+    }
+  };
 
-	$scope.filterTask = function (type) {
-		switch (type) {
-			case 'All':
-				$scope.condition = '';
-				break;
-
-			case 'Active':
-				$scope.condition = false;
-				console.log('筛选结果' + $scope.condition)
-				break;
-
-			case 'Completed':
-				$scope.condition = true;
-				break;
-
-		}
-
-	}
-
-	// 6.计算未完成的数量
-	$scope.calcNumber = function () {
-		var result = 0;
-		for (var i = 0; i < $scope.taskList.length; i++) {
-			if (!$scope.taskList[i].isCompleted) {
-				result++;
-			}
-		}
-		return result;
-	}
-	// 7.清空已完成的任务
-	$scope.clearData = function () {
-		for (var i = 0; i < $scope.taskList.length; i++) {
-			if ($scope.taskList[i].isCompleted) {
-				$scope.taskList.splice(i, 1)
-				i--;
-			}
-		}
-	}
-
-
-	// 8.批量修改任务状态
-	// 给全选按钮绑定一个变量，使用变量的值判断
-	$scope.changeStatus = function () {
-		for (var i = 0; i < $scope.taskList.length; i++) {
-
-			$scope.taskList[i].isCompleted = $scope.status;
-
-		}
-	}
-	// 9.修改任务名称
-	$scope.modifyData = function (id) {
-		for (var i = 0; i < $scope.taskList.length; i++) {
-
-			if ($scope.taskList[i].id == id) {
-				$scope.taskList[i].isEdit = true;
-
-			} else {
-				$scope.taskList[i].isEdit = false;
-			}
-		}
-	}
-	$scope.blurFn = function () {
-		for (var i = 0; i < $scope.taskList.length; i++) {
-			$scope.taskList[i].isEdit = false;
-
-		}
-
-	}
-
-
+  // 8.批量修改任务状态
+  // 给全选按钮绑定一个变量，使用变量的值判断
+  $scope.changeStatus = function() {
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      $scope.taskList[i].isCompleted = $scope.status;
+    }
+  };
+  // 9.修改任务名称
+  $scope.modifyData = function(id) {
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      if ($scope.taskList[i].id == id) {
+        $scope.taskList[i].isEdit = true;
+      } else {
+        $scope.taskList[i].isEdit = false;
+      }
+    }
+  };
+  $scope.blurFn = function() {
+    for (var i = 0; i < $scope.taskList.length; i++) {
+      $scope.taskList[i].isEdit = false;
+    }
+  };
 }
---
+++ react/precache-manifest.82e9e5ca166a34ec1e0fc87379386f91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ae9e49fd85edaf39c9f575ee3895bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8db1a278cfe86b787df3",
+    "url": "/static/css/main.4d8fcd1c.chunk.css"
+  },
+  {
+    "revision": "64d3f386f1ea1b43b844",
+    "url": "/static/js/2.e3f7a60d.chunk.js"
+  },
+  {
+    "revision": "8db1a278cfe86b787df3",
+    "url": "/static/js/main.67425ced.chunk.js"
+  },
+  {
+    "revision": "94183b026903fbc073a7",
+    "url": "/static/js/runtime-main.0aa19eb4.js"
+  }
+]);
